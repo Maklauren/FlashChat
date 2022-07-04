2d16cec1b0dd5139d6e19698e9c98a959ea94c2b
Pavel Chernook <Maklauren97@gmail.com>
2022-07-04 18:01:45 +0300

    Initial commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Павел Черноок on 4.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
