2534079216d219916e238be8a9d9fb757ef196fb
Pavel Chernook <Maklauren97@gmail.com>
2022-07-06 13:07:59 +0300

    Implemented user logout logic

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var messageTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "⚡️FlashChat"
+        
+        navigationItem.hidesBackButton = true
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
 }
