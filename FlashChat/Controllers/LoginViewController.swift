78276bd64d3b9b479221e613cad861a24dbe63e7
Pavel Chernook <Maklauren97@gmail.com>
2022-07-06 12:53:30 +0300

    Implemented the authorization logic

--- FlashChat/Controllers/LoginViewController.swift
+++ FlashChat/Controllers/LoginViewController.swift
@@ -6,11 +6,31 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    let alert = UIAlertController(title: "We are sorry.", message: error.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
 }
